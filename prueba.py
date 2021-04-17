8df6ab25a4a3b78db254134799109b99f2bacc42
Nahuel Olivera <nahuolivera55@gmail.com>
2021-04-17 19:48:36 -0300

    Created a 'Palindromo' version of prueba.py

--- prueba.py
+++ prueba.py
@@ -1,9 +1,13 @@
 def run():
     palabra = input("Escriba una palabra: ")
-    print(palabra)
-    
+    palabra = palabra.replace (" ", "")
+    palabra_invertida = palabra[::-1]
+    if palabra == palabra_invertida:
+        print("Es palindromo.")
+    else:
+        print("No es palindromo.")
 
 
 
 if __name__ == "__main__":
-    run() 
+    run()
