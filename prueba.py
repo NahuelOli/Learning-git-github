37e5b2c99385bba0a2c59f081e376ab70c115db6
Nahuel Olivera <nahuolivera55@gmail.com>
2021-04-21 17:57:09 -0300

    solving fucking conflicts

--- prueba.py
+++ prueba.py
@@ -1,6 +1,6 @@
 def run():
-    print("""'Bienvenido al averiguador capicua'   
-    """)
+   
+   
     palabra = input("Escriba una palabra: ")
     palabra = palabra.replace (" ", "")
     palabra_invertida = palabra[::-1]
