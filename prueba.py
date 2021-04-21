b6f0e5496b133f42e76995c5ef7e30a548845c8e
Nahuel Olivera <nahuolivera55@gmail.com>
2021-04-21 17:36:10 -0300

    Now you can either write a word or a number

--- prueba.py
+++ prueba.py
@@ -1,5 +1,5 @@
 def run():
-    palabra = input("Escriba una palabra: ")
+    palabra = input("Escriba una palabra o un numero: ")
     palabra = palabra.replace (" ", "")
     palabra_invertida = palabra[::-1]
     if palabra == palabra_invertida:
