76b92e3dc307b000289d7752c99d8048fdbea298
Nahuel Olivera <nahuolivera55@gmail.com>
2021-04-23 20:38:34 -0300

    Mensaje chingon en español

--- prueba.py
+++ prueba.py
@@ -19,7 +19,15 @@ def run():
         print("El numero " + str(numero) + " es par.")
     else:
         print("El numero " + str(numero) + " es impar.")
+    
 
+    print("""
+    Hola pendejo qlo, 
+    este es un mensaje bien chingon 
+    para que te rias 
+    al final del programa 
+    jejeje
+    """)
 
 if __name__ == "__main__":
     run()
